b4269fe864d5687cc6de181bd9cb2a411b8be04b
JKorf <jankorf91@gmail.com>
2026-03-22 22:05:13 +0100

    wip

--- Bitstamp.Net/Extensions/ServiceCollectionExtensions.cs
+++ Bitstamp.Net/Extensions/ServiceCollectionExtensions.cs
@@ -105,8 +105,6 @@ namespace Microsoft.Extensions.DependencyInjection
             }).SetHandlerLifetime(Timeout.InfiniteTimeSpan);
             services.Add(new ServiceDescriptor(typeof(IBitstampSocketClient), x => { return new BitstampSocketClient(x.GetRequiredService<IOptions<BitstampSocketOptions>>(), x.GetRequiredService<ILoggerFactory>()); }, socketClientLifeTime ?? ServiceLifetime.Singleton));
 
-            services.AddTransient<ICryptoRestClient, CryptoRestClient>();
-            services.AddTransient<ICryptoSocketClient, CryptoSocketClient>();
             services.AddTransient<IBitstampOrderBookFactory, BitstampOrderBookFactory>();
             services.AddTransient<IBitstampTrackerFactory, BitstampTrackerFactory>();
             services.AddTransient<ITrackerFactory, BitstampTrackerFactory>();
